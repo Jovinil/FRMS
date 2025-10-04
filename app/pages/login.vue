bb3e7aeaeb83dd212c89f78a082eec3846e784fe
whiteAndBlackPanda <142206766+camachoRamel@users.noreply.github.com>
2025-10-04 14:25:39 +0800

    models

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -2,6 +2,16 @@
 definePageMeta({
   layout: 'empty'
 })
+const client = useSupabaseClient()
+const email = ref('')
+const password = ref('')
+
+const handleLogin = async () => {
+    const { data, error} = await client.auth.signInWithPassword({
+        email: email.value,
+        password: password.value
+    }) 
+}
 </script>
 
 <template>
@@ -11,18 +21,18 @@ definePageMeta({
         class="absolute inset-0 h-1/2 object-contain m-auto" 
     />
     
-    <div class="flex flex-col items-center justify-center h-full gap-20">
+    <form @submit.prevent="handleLogin" class="flex flex-col items-center justify-center h-full gap-20">
         <p class="z-10 text-2xl font-bold">ADMIN LOGIN</p>
         <div class="flex flex-col gap-15 w-full items-center">
-            <UInput placeholder="Email" class="w-1/2" />
-            <UInput placeholder="Password" class="w-1/2" />
+            <UInput name="email" type="email" placeholder="Email" class="w-1/2" />
+            <UInput name="password" type="password" placeholder="Password" class="w-1/2" />
         </div>
       
 
         <div class=" w-1/2 flex items-center justify-center z-10 mt-">
-            <UButton class="w-1/2" color="primary" variant="solid" :ui="{base: 'justify-center'}">Login</UButton>
+            <UButton type="submit" class="w-1/2" color="primary" variant="solid" :ui="{base: 'justify-center'}">Login</UButton>
         </div>
-    </div>
+    </form>
 
     </div>
 </template>
